6fcec694c61c3eef2fc74bf5f83a7f43a00a176a
Charles <charleslee522@gmail.com>
2017-04-24 11:11:50 +0900

    포트 수정

--- server/MasterOfTime/controller/AlarmManager.js
+++ server/MasterOfTime/controller/AlarmManager.js
@@ -196,7 +196,7 @@ function AlarmManager() {
   var createJob = function (alarmName) {
     alarms[alarmName].job = schedule.scheduleJob(alarms[alarmName].time, function () {
       request.post({
-        url: 'http://localhost:3000/alarm',
+        url: 'http://localhost:8000/alarm',
         body: {
           desc: alarms[alarmName].desc,
           alarmName: alarmName,
