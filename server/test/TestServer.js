6417940e35af2fc48b0c8e3b751aad1148de24d4
Charles <charleslee522@gmail.com>
2017-05-20 22:24:29 +0900

    야구 숫자 사이 separator 추가

--- server/test/TestServer.js
+++ server/test/TestServer.js
@@ -133,3 +133,58 @@ describe('Server POST /hook', function() {
       .expect(200, done)
   });
 });
+
+
+describe('Baseball Server Test', function() {
+  it('야구 시작', function(done) {
+    request(app)
+      .post('/hook')
+      .send(
+        {
+            "events": [
+                {
+                    "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+                    "type": "message",
+                    "timestamp": 1462629479859,
+                    "source": {
+                        "type": "user",
+                        "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+                    },
+                    "message": {
+                        "id": "325708",
+                        "type": "text",
+                        "text": '@야구 시작'
+                    }
+                }
+            ]
+        }
+      )
+      .expect(200, done)
+  });
+
+  it('야구 플레이', function(done) {
+    request(app)
+      .post('/hook')
+      .send(
+        {
+            "events": [
+                {
+                    "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+                    "type": "message",
+                    "timestamp": 1462629479859,
+                    "source": {
+                        "type": "user",
+                        "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+                    },
+                    "message": {
+                        "id": "325708",
+                        "type": "text",
+                        "text": '@야구 1  41 9'
+                    }
+                }
+            ]
+        }
+      )
+      .expect(200, done)
+  });
+});
